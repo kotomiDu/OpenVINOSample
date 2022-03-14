07b140dfe0518ef0bc121f2cd519a3be0bea7988
YaruDu <yaru.du@intel.com>
2022-03-14 18:34:13 +0800

    Initialize third-party package:openvino, tbb, opencv

---
+++ third-party/opencv/cmake/OpenCVModules-debug.cmake
@@ -0,0 +1,161 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "opencv_core" for configuration "Debug"
+set_property(TARGET opencv_core APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_core PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_core452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_core452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_core )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_core "${_IMPORT_PREFIX}/lib/opencv_core452d.lib" "${_IMPORT_PREFIX}/bin/opencv_core452d.dll" )
+
+# Import target "opencv_flann" for configuration "Debug"
+set_property(TARGET opencv_flann APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_flann PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_flann452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_flann452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_flann )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_flann "${_IMPORT_PREFIX}/lib/opencv_flann452d.lib" "${_IMPORT_PREFIX}/bin/opencv_flann452d.dll" )
+
+# Import target "opencv_imgproc" for configuration "Debug"
+set_property(TARGET opencv_imgproc APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_imgproc PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_imgproc452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_imgproc452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_imgproc )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_imgproc "${_IMPORT_PREFIX}/lib/opencv_imgproc452d.lib" "${_IMPORT_PREFIX}/bin/opencv_imgproc452d.dll" )
+
+# Import target "opencv_ml" for configuration "Debug"
+set_property(TARGET opencv_ml APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_ml PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_ml452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_ml452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_ml )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_ml "${_IMPORT_PREFIX}/lib/opencv_ml452d.lib" "${_IMPORT_PREFIX}/bin/opencv_ml452d.dll" )
+
+# Import target "opencv_photo" for configuration "Debug"
+set_property(TARGET opencv_photo APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_photo PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_photo452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_photo452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_photo )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_photo "${_IMPORT_PREFIX}/lib/opencv_photo452d.lib" "${_IMPORT_PREFIX}/bin/opencv_photo452d.dll" )
+
+# Import target "opencv_dnn" for configuration "Debug"
+set_property(TARGET opencv_dnn APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_dnn PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_dnn452d.lib"
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_DEBUG "IE::inference_engine;IE::inference_engine_c_api;ngraph::ngraph"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_dnn452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_dnn )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_dnn "${_IMPORT_PREFIX}/lib/opencv_dnn452d.lib" "${_IMPORT_PREFIX}/bin/opencv_dnn452d.dll" )
+
+# Import target "opencv_features2d" for configuration "Debug"
+set_property(TARGET opencv_features2d APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_features2d PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_features2d452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_features2d452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_features2d )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_features2d "${_IMPORT_PREFIX}/lib/opencv_features2d452d.lib" "${_IMPORT_PREFIX}/bin/opencv_features2d452d.dll" )
+
+# Import target "opencv_imgcodecs" for configuration "Debug"
+set_property(TARGET opencv_imgcodecs APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_imgcodecs PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_imgcodecs452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_imgcodecs452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_imgcodecs )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_imgcodecs "${_IMPORT_PREFIX}/lib/opencv_imgcodecs452d.lib" "${_IMPORT_PREFIX}/bin/opencv_imgcodecs452d.dll" )
+
+# Import target "opencv_videoio" for configuration "Debug"
+set_property(TARGET opencv_videoio APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_videoio PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_videoio452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_videoio452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_videoio )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_videoio "${_IMPORT_PREFIX}/lib/opencv_videoio452d.lib" "${_IMPORT_PREFIX}/bin/opencv_videoio452d.dll" )
+
+# Import target "opencv_calib3d" for configuration "Debug"
+set_property(TARGET opencv_calib3d APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_calib3d PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_calib3d452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_calib3d452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_calib3d )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_calib3d "${_IMPORT_PREFIX}/lib/opencv_calib3d452d.lib" "${_IMPORT_PREFIX}/bin/opencv_calib3d452d.dll" )
+
+# Import target "opencv_highgui" for configuration "Debug"
+set_property(TARGET opencv_highgui APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_highgui PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_highgui452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_highgui452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_highgui )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_highgui "${_IMPORT_PREFIX}/lib/opencv_highgui452d.lib" "${_IMPORT_PREFIX}/bin/opencv_highgui452d.dll" )
+
+# Import target "opencv_objdetect" for configuration "Debug"
+set_property(TARGET opencv_objdetect APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_objdetect PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_objdetect452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_objdetect452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_objdetect )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_objdetect "${_IMPORT_PREFIX}/lib/opencv_objdetect452d.lib" "${_IMPORT_PREFIX}/bin/opencv_objdetect452d.dll" )
+
+# Import target "opencv_stitching" for configuration "Debug"
+set_property(TARGET opencv_stitching APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_stitching PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_stitching452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_stitching452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_stitching )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_stitching "${_IMPORT_PREFIX}/lib/opencv_stitching452d.lib" "${_IMPORT_PREFIX}/bin/opencv_stitching452d.dll" )
+
+# Import target "opencv_video" for configuration "Debug"
+set_property(TARGET opencv_video APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_video PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_video452d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_video452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_video )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_video "${_IMPORT_PREFIX}/lib/opencv_video452d.lib" "${_IMPORT_PREFIX}/bin/opencv_video452d.dll" )
+
+# Import target "opencv_gapi" for configuration "Debug"
+set_property(TARGET opencv_gapi APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_gapi PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/opencv_gapi452d.lib"
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_DEBUG "IE::inference_engine;IE::inference_engine_c_api"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/opencv_gapi452d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_gapi )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_gapi "${_IMPORT_PREFIX}/lib/opencv_gapi452d.lib" "${_IMPORT_PREFIX}/bin/opencv_gapi452d.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
